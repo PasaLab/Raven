127b2cb2b1fa974105b24cba81abbf5805c35a2c
Violape <dz20330029@smail.nju.edu.cn>
2021-05-21 15:02:59 +0800

    - changing the method of data generation via s3

--- workloads/tpch.py
+++ workloads/tpch.py
@@ -11,18 +11,27 @@ class tpch(workload):
         self.logger = Logger('./log/benchmark.log', __name__)
 
     def generate(self):
-        command = 'cd ' + self.conf['generate']['path'] + ' && ' + self.conf['generate']['command']
-        subprocess_popen(command)
+        commands = self.conf['generate']['commands']
+        command_seq = ""
+        command_first = True
+        for command in commands:
+            if command_first:
+                command_seq = command_seq + command
+                command_first = False
+            else:
+                command_seq = command_seq + " && " + command
+        subprocess_popen(command_seq)
 
     def create(self, engine):
         for sql in self.conf['create']['sql']:
             engine.query(sql)
 
     def load(self, engine):
-        src = self.conf['load']['path']
+        pre_command = self.conf['load']['pre_command']
+        subprocess_popen(pre_command)
         tables = self.conf['load']['tables']
         for table in tables:
-            sql = "LOAD DATA LOCAL INPATH '" + src + "/" + table['load'] + "' INTO TABLE " + table['as']
+            sql = "LOAD DATA LOCAL INPATH './" + table['load'] + "' INTO TABLE " + table['as']
             engine.query(sql)
             self.logger.info("Successfully uploaded " + table['load'] + "as" + table['as'] + ".")
 
