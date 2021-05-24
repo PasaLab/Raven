8371ffe5e0b617305400e979f6a130142b165524
Violape <dz20330029@smail.nju.edu.cn>
2021-05-24 14:18:23 +0800

    update prepare.sh

--- cloud/prepare.sh
+++ cloud/prepare.sh
@@ -1,3 +1,4 @@
+# on master
 sudo yum -y install git
 sudo python3 -m pip install boto3
 sudo python3 -m pip install pyspark
@@ -9,19 +10,25 @@ make
 cd ~
 git clone https://github.com/PasaLab/OLAPBenchmark
 
+# on slaves
+sudo yum -y install git
+git clone https://github.com/PasaLab/OLAPBenchmark
+
+# on all machines
 sudo yum -y install collectd
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
 
+# on master
 cd ~/OLAPBenchmark
 python3 main.py generate
 
-sudo yum -y install git
-git clone https://github.com/PasaLab/OLAPBenchmark
-
+# on all machines
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/hadoop/OLAPBenchmark/cloud/cwaconfig.json
 
+# on master
 cd ~/OLAPBenchmark
 python3 main.py run
 
+# on all machines
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a stop
