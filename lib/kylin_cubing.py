843e120a932156855e9aa74c093b2841495e4214
Violape <dz20330029@smail.nju.edu.cn>
2021-06-01 08:21:09 +0800

    allowing selection of cubes to build

--- lib/kylin_cubing.py
+++ lib/kylin_cubing.py
@@ -1,8 +1,17 @@
 import time
 import json
 import requests
+import sys
+from lib.Logger import Logger
 
-url = "http://localhost:7070/kylin/api/cubes/TPCH_Q1/build"
+logger = Logger('./log/benchmark.log', 'kylin_cubing')
+cube_name = ""
+if len(sys.argv) == 2:
+    cube_name = sys.argv[1]
+else:
+    logger.error("Invalid argument. Please give a valid cube name to build")
+
+url = "http://localhost:7070/kylin/api/cubes/" + cube_name + "/build"
 payload = "{\"buildType\":\"BUILD\",\"startTime\":0,\"endTime\":0}"
 headers = {
     'Authorization': "Basic QURNSU46S1lMSU4=",
@@ -10,13 +19,13 @@ headers = {
 }
 response = requests.request("PUT", url, data=payload, headers=headers)
 uuid = json.loads(response.text)['uuid']
-print(uuid)
+logger.info("Build request sent, uuid is " + uuid)
 url = "http://localhost:7070/kylin/api/jobs"
 querystring = {"jobSearchMode":"ALL","limit":"15","offset":"0","projectName":"tpch","timeFilter":"2"}
 headers = {'Authorization': "Basic QURNSU46S1lMSU4="}
 message = {"uuid": uuid, "job_status": 'PENDING', "progress": 0}
-print(message['job_status'], message['progress'])
-while message['job_status'] != 'FAILED' or 'FINISHED':
+logger.info("Build is " + message['job_status'] + ". Progress is " + str(round(message['progress'], 1)))
+while message['job_status'] != 'FINISHED':
     time.sleep(30)
     response = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)
     message = None
@@ -25,7 +34,7 @@ while message['job_status'] != 'FAILED' or 'FINISHED':
             message = item
             break
     if message is None:
-        print("uuid not found!")
-        message = {'job_status': 'FAILED'}
+        logger.error("uuid not found!")
+        message = {'job_status': 'FINISHED'}
     else:
-        print(message['job_status'], message['progress'])
+        logger.info("Build is " + message['job_status'] + ". Progress is " + str(round(message['progress'], 1)))
