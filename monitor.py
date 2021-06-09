fdfdf3aad0ac7d516d279ccb5fb868e643aa10a8
Violape <dz20330029@smail.nju.edu.cn>
2021-06-09 21:08:32 +0800

    radar

--- monitor.py
+++ monitor.py
@@ -4,6 +4,7 @@ import yaml
 import numpy as np
 from lib.boto3sdk import download
 from lib.Logger import Logger
+import matplotlib.pyplot as plt
 
 
 def get_metrics(cid, start, end):
@@ -109,16 +110,16 @@ def analyze(metrics, timestamps, start, finish):
 
     # A3. Query time distribution
     if 'time_between_queries' in conf['metrics']:
-        time_tot_online = 0
-        query_start = 0
-        query_finish = 0
+        temp_time_tot_online = 0
+        temp_query_start = 0
+        temp_query_finish = 0
         for query in timestamps['online']:
-            if query['finish'] > query_finish:
-                query_finish = query['finish']
-            if query['start'] < query_start or query_start == 0:
-                query_start = query['start']
-            time_tot_online += query['finish'] - query['start']
-        time_between_queries = query_finish - query_start - time_tot_online
+            if query['finish'] > temp_query_finish:
+                temp_query_finish = query['finish']
+            if query['start'] < temp_query_start or temp_query_start == 0:
+                temp_query_start = query['start']
+            temp_time_tot_online += query['finish'] - query['start']
+        time_between_queries = temp_query_finish - temp_query_start - temp_time_tot_online
     if 'time_variation_per_query' in conf['metrics']:
         time_variation_per_query = np.std(online_times)
 
@@ -128,89 +129,89 @@ def analyze(metrics, timestamps, start, finish):
     # B. Resource Usage
     # B1. used resources & free time
     if 'cpu_avg_online' in conf['metrics'] or 'cpu_free_time' in conf['metrics']:
-        cpu_avg_online = 0
-        cpu_avg_online_cnt = 0
+        temp_cpu_avg_online = 0
+        temp_cpu_avg_online_cnt = 0
         for instance in metrics:
             for metric in instance:
                 if metric['Label'] == "cpu_usage_user":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            cpu_avg_online += metric['Values'][i]
-                            cpu_avg_online_cnt += 1
-        cpu_avg_online = cpu_avg_online / cpu_avg_online_cnt * 0.01
+                            temp_cpu_avg_online += metric['Values'][i]
+                            temp_cpu_avg_online_cnt += 1
+        cpu_avg_online = temp_cpu_avg_online / temp_cpu_avg_online_cnt * 0.01
         cpu_free_time = (finish - start) * (1 - cpu_avg_online)
     if 'mem_avg_online' in conf['metrics'] or 'mem_free_time' in conf['metrics']:
-        mem_avg_online = 0
-        mem_avg_online_cnt = 0
+        temp_mem_avg_online = 0
+        temp_mem_avg_online_cnt = 0
         for instance in metrics:
             for metric in instance:
                 if metric['Label'] == "mem_used_percent":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            mem_avg_online += metric['Values'][i]
-                            mem_avg_online_cnt += 1
-        mem_avg_online = mem_avg_online / mem_avg_online_cnt * 0.01
+                            temp_mem_avg_online += metric['Values'][i]
+                            temp_mem_avg_online_cnt += 1
+        mem_avg_online = temp_mem_avg_online / temp_mem_avg_online_cnt * 0.01
         mem_free_time = (finish - start) * (1 - mem_avg_online)
 
     # B2. Load balance
     if 'cpu_load_balance' in conf['metrics']:
-        cpu_average_loads = []
+        temp_cpu_average_loads = []
         for instance in metrics:
-            cpu_avg_online = 0
-            cpu_avg_online_cnt = 0
+            temp_cpu_avg_online = 0
+            temp_cpu_avg_online_cnt = 0
             for metric in instance:
                 if metric['Label'] == "cpu_usage_user":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            cpu_avg_online += metric['Values'][i]
-                            cpu_avg_online_cnt += 1
-            cpu_average_loads.append(cpu_avg_online / cpu_avg_online_cnt * 0.01)
-        cpu_load_balance = np.std(cpu_average_loads)
+                            temp_cpu_avg_online += metric['Values'][i]
+                            temp_cpu_avg_online_cnt += 1
+            temp_cpu_average_loads.append(temp_cpu_avg_online / temp_cpu_avg_online_cnt * 0.01)
+        cpu_load_balance = np.std(temp_cpu_average_loads)
 
     if 'mem_load_balance' in conf['metrics']:
-        mem_average_loads = []
+        temp_mem_average_loads = []
         for instance in metrics:
-            mem_avg_online = 0
-            mem_avg_online_cnt = 0
+            temp_mem_avg_online = 0
+            temp_mem_avg_online_cnt = 0
             for metric in instance:
                 if metric['Label'] == "mem_used_percent":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            mem_avg_online += metric['Values'][i]
-                            mem_avg_online_cnt += 1
-            mem_average_loads.append(mem_avg_online / mem_avg_online_cnt * 0.01)
-        mem_load_balance = np.std(mem_average_loads)
+                            temp_mem_avg_online += metric['Values'][i]
+                            temp_mem_avg_online_cnt += 1
+            temp_mem_average_loads.append(temp_mem_avg_online / temp_mem_avg_online_cnt * 0.01)
+        mem_load_balance = np.std(temp_mem_average_loads)
 
     # B3. I/O efficiency
     if 'io_avg_time' in conf['metrics']:
-        io_avg_time = 0
-        io_avg_online_cnt = 0
+        temp_io_avg_time = 0
+        temp_io_avg_online_cnt = 0
         for instance in metrics:
             for metric in instance:
                 if metric['Label'] == "io_time":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            io_avg_time += metric['Values'][i]
-                            io_avg_online_cnt += 1
-        io_avg_time = io_avg_time / io_avg_online_cnt * 0.01
+                            temp_io_avg_time += metric['Values'][i]
+                            temp_io_avg_online_cnt += 1
+        io_avg_time = temp_io_avg_time / temp_io_avg_online_cnt * 0.01
 
     if 'disk_usage' in conf['metrics']:
-        disk_usage = 0
-        disk_usage_cnt = 0
+        temp_disk_usage = 0
+        temp_disk_usage_cnt = 0
         for instance in metrics:
             for metric in instance:
                 if metric['Label'] == "disk_used":
                     for i in range(len(metric['Timestamps'])):
                         time = metric['Timestamps'][i]
                         if start <= time <= finish:
-                            disk_usage += metric['Values'][i]
-                            disk_usage_cnt += 1
-        disk_usage = disk_usage / disk_usage_cnt * 0.01
+                            temp_disk_usage += metric['Values'][i]
+                            temp_disk_usage_cnt += 1
+        disk_usage = temp_disk_usage / temp_disk_usage_cnt * 0.01
 
     # C. Bill prediction
 
@@ -218,6 +219,21 @@ def analyze(metrics, timestamps, start, finish):
     offline_delay_overhead = eval(str(conf['offline']['delay']['eval']))
     online_calculation_overhead = eval(str(conf['online']['calculation']['eval']))
     online_delay_overhead = eval(str(conf['online']['delay']['eval']))
+    # name = np.array(['Offline calculation', 'Offline delay', 'Online calculation', 'Online delay'])
+    # value = np.array([offline_calculation_overhead, offline_delay_overhead, online_calculation_overhead, online_delay_overhead])
+    # theta = np.concatenate((theta, [theta[0]]))
+    # value = np.concatenate((value, [value[0]]))
+    name = np.array(['CPU idle', 'Memory idle', 'CPU skew', 'Memory skew'])
+    theta = np.linspace(0, 2 * np.pi, len(name), endpoint=False)
+    value = np.array([1 - cpu_avg_online, 1 - mem_avg_online, cpu_load_balance, mem_load_balance])
+    ax = plt.subplot(111, projection='polar')
+    ax.plot(theta, value, 'm-', lw=1, alpha=0.75)
+    ax.fill(theta, value, 'm', alpha=0.75)
+    ax.set_thetagrids(theta * 180 / np.pi, name)
+    ax.set_theta_zero_location('N')
+    ax.set_ylim(0,1)
+    ax.set_title('Benchmark Results - resources', fontsize=20)
+    plt.show()
     logger.info("--------------------------------")
     logger.info("Offline calculation overhead: " + str(round(offline_calculation_overhead, 3)))
     logger.info("Offline delay overhead: " + str(round(offline_delay_overhead, 3)))
